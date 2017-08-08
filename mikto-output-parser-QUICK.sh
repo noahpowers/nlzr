fd35da2f5184873add0d583555f5579dabe5a7f8
owz <npowers@delta-risk.net>
2017-08-08 17:52:35 -0500

    Update mikto-output-parser-QUICK.sh

--- mikto-output-parser-QUICK.sh
+++ mikto-output-parser-QUICK.sh
@@ -6,4 +6,19 @@
 ##
 ## writes a file called 'results.txt' where a semi-useful output with files is found
 
-for y in $(ls);do for z in $(grep -E -i "/" $y |grep -E -v "Server:|requests|PROPFIND" | sed -e 's/:\s*/:\ /' | sed -e 's/OSVDB-[0-9]\{3,5\}://' | cut -d" " -f3 | cut -d":" -f1 | grep "\.");do echo `echo $y$z` >> text.txt;done;done; uniq text.txt > results.txt | sed -i 's/\.txt\//\//' results.txt; sed -i 's/-/:/g' results.txt;rm text.txt
+for y in $(ls)
+    do 
+    for z in $(grep -E -i "/" $y |grep -E -v "Server:|requests|PROPFIND" | sed -e 's/:\s*/:\ /' | sed -e 's/OSVDB-[0-9]\{3,5\}://' | cut -d" " -f3 | cut -d":" -f1 | grep "\.")
+        do echo `echo $y$z` >> text.txt
+        done
+    done
+    
+uniq text.txt > results.txt  
+sed -i 's/\.txt\//\//' results.txt
+sleep 2
+sed -i 's/-/:/g' results.txt
+sleep 2
+sed -i 's/cgi:bin/cgi-bin/g' results.txt
+sleep 2
+sed -i 's/cgi:local/cgi-local/g' results.txt
+rm text.txt
