1b9d5509f0788f2446bef2ccb64e61b191adc460
owz <npowers@delta-risk.net>
2017-09-12 14:06:54 -0500

    Update visitors.sh

--- visitors.sh
+++ visitors.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-for z in $(cat /var/log/apache2/access.log | cut -d " " -f 1); do
-    curl -s ipinfo.io/$z | grep -E "\{|ip|org|\}"
-    echo $'\n'
-done
+for z in $(cat /var/log/apache2/access.log | cut -d " " -f 1 | sort | uniq); do curl -s ipinfo.io/$z | grep -E "\{|ip|org|\}"; echo $'\n'; done
