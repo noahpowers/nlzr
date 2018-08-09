0322d99751b6686f26697d7a89fdf368ccfc510a
owz <npowers@delta-risk.net>
2018-08-08 21:37:42 -0500

    Update recon.sh

--- reconnaissance/recon.sh
+++ reconnaissance/recon.sh
@@ -3,32 +3,38 @@
 ### git clone https://github.com/aboul3la/Sublist3r ; cd Sublist3r/ ; pip install -r requirements.txt 
 ### Run script from the Sublist3r folder!!
 function install-Sublist3r() {
+    dir=$(pwd)
+    cd ~
     git clone https://github.com/aboul3la/Sublist3r
     cd Sublist3r/
     pip install -r requirements.txt
-    cd ..
-
+    cd $dir
 }
 
 function install-SimplyEmail() {
+    dir=$(pwd)
+    cd ~
     git clone https://github.com/killswitch-GUI/SimplyEmail
     cd SimplyEmail
     bash setup/setup.sh
+    cd $dir
 }
 
 function gatherDomains() {
     read -p "Enter a domain: " -r name
-    cp subdomains.py Sublist3r/
-    cd Sublist3r/
+    dir=$(pwd)
+    cd ~
+    subdomainFile=$(find -name "subdomains.py")
+    sublist3rLocation=$(find -name "Sublist3r")
+    cp $subdomainFile $sublist3rLocation
+    cd $sublist3rLocation
     echo "#,Domain Name,IP Address" > ${name}-DomainNames.csv
     echo "Subnet,Number of Names Discovered,Min IP,Max IP" > ${name}-DomainSubnets.csv
     ipaddr=$(nslookup $name | grep "Address" | grep -iv "#" | cut -d" " -f2)
     count=1
     echo "$count,$name,$ipaddr" >> ${name}-DomainNames.csv
-
     python subdomains.py $name
     echo "$name" >> ${name}_subdomains.txt
-
     for subname in $(cat ${name}_subdomains.txt);
     do
         count=$[count + 1]
@@ -38,20 +44,25 @@ function gatherDomains() {
         echo "$count,$subname,$ipaddr" >> ${name}-DomainNames.csv
         echo "$cidr,$subname,," >> ${name}-DomainSubnets.csv
     done
-    cd ..
+    cd ~
+    sublist3rLocation=$(find -name "Sublist3r")
+    echo "File stored in directory: $sublist3rLocation"
+    cd $dir
 }
 
 function findEmails() {
     domainfile="/root/*_subdomains.txt.txt"
     read -p "Enter Path to File Containing Domains [full path]: " -r targetsfile
-    cd SimplyEmail/
+    dir=$(pwd)
+    cd ~
+    path=$(find -name SimplyEmail)
+    cd $path
     for z in $(cat $targetsfile);
-    path=$(pwd)
-    do 
+        do 
         python SimplyEmail.py -e $z -all --json ${z}-emails.txt
     done
     echo "[+] Email files stored in path: ${path}"
-    cd ..
+    cd $dir
 }
 
 PS3="Server Setup Script - Pick an option: "
@@ -73,5 +84,4 @@ select opt in "${options[@]}" "Quit"; do
     *) echo "Invalid option. Try another one.";continue;;
 
     esac
-
 done
