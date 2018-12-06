637e06fb18b49b6dc3d25bc1bed60dee6084e396
owz <npowers@delta-risk.net>
2018-12-06 18:29:23 +0000

    Create hibp.py

---
+++ reconnaissance/hibp.py
@@ -0,0 +1,32 @@
+from hibpAPI import *
+
+begin = time.time()
+
+## hard-coded email address for testing purposes
+#email = "person@somedomain.com"
+
+auth = AuthToken()
+prop = ID()
+
+### [resource = ] Sets API URI
+emailCount = 0
+breachList = []
+methods = Methods()
+with open(sys.argv[1]) as f:
+    for line in f:
+        line = line.strip()
+        resource = methods.setAttr('breachedaccount/' + line)
+        emailresults = methods.GET(resource)
+        if emailresults is not "false":
+            emailCount += 1
+# ### obtaining breach information for 
+            breachData = prop.getBreachAccountDetails(emailresults, line)
+            breachList.append(breachData)
+
+timestr = str(time.strftime("%Y%m%d-%H%M%S"))
+filepath = "/root/breachedaccounts-" + timestr + ".csv"
+prop.obtainCSV(breachList,filepath,emailCount)
+
+end = time.time()
+### time it took in seconds
+print(round(end - begin))
