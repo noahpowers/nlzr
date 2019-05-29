369fa47d4f880abacd5a067cce95b5d86284d8c9
owz <powers.noah@att.net>
2019-05-29 22:10:17 +0000

    Update statusLateralMovement.py

--- statusLateralMovement.py
+++ statusLateralMovement.py
@@ -41,22 +41,22 @@ while count < (len(list) - 1):
         fromIP= list[count][1]
         spawnPID_temp = list[count][2].split('.log:')[0]
         spawnPID = spawnPID_temp.split('_')[1]
+        btime = list[count][3].split(' ')[1]
         reason = list[count][2].split('.log:')[1]
+        operator_t0 = list[count][3].split(' ')[4]
+        operator_t1 = operator_t0.split('<')[1]
+        operator = operator_t1.split('>')[0]
         command = list[count][3].split('> ')[1]
-        interimList.append([date,spawnPID,fromIP,command])
+        interimList.append([date,btime,spawnPID,fromIP,command,operator])
         count += 1
 count = 0
 
 print ""
-print '''
-Beacons:
-'''
-print 
 print ''' 
-################################################################################
-   DTG           Spawn-PID              FROM-IP                 To-HOSTNAME
---------------------------------------------------------------------------------
-################################################################################
+#########################################################################################
+   DATE     TIME       Spawn-PID     FROM-IP            To-HOSTNAME          OPERATOR
+----------------------------------------------------------------------------------------
+#########################################################################################
 '''
 
 for item in interimList:
